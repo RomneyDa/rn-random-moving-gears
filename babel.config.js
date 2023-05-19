cd8fe8772a27e8c7cf9cdfa1cffea8df4e519ea0
Dallin Romney <dallinromney@gmail.com>
2023-05-18 21:27:02 -0600

    babel config fixes

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,41 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    alias: {
-      '^@src/(.+)': './src/\\1',
-      '^@assets/(.+)': './src/assets/\\1',
-      '^@components/(.+)': './src/components/\\1',
-      '^@contexts/(.+)': './src/contexts/\\1',
-      '^@models/(.+)': './src/models/\\1',
-      '^@navigation/(.+)': './src/navigation/\\1',
-      '^@screens/(.+)': './src/screens/\\1',
-      '^@services/(.+)': './src/services/\\1',
-      '^@utils/(.+)': './src/utils/\\1',
-    },
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            '^@src/(.+)': './src/\\1',
+            '^@assets/(.+)': './src/assets/\\1',
+            '^@components/(.+)': './src/components/\\1',
+            '^@contexts/(.+)': './src/contexts/\\1',
+            '^@models/(.+)': './src/models/\\1',
+            '^@navigation/(.+)': './src/navigation/\\1',
+            '^@screens/(.+)': './src/screens/\\1',
+            '^@services/(.+)': './src/services/\\1',
+            '^@utils/(.+)': './src/utils/\\1',
+          },
+        },
+      ],
+      [
+        'react-native-reanimated/plugin',
+        {
+          globals: ['__scanCodes'],
+        },
+      ],
+    ],
   }
 }
