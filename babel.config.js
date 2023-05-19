a5ed1ef7f663ae8d7480199acb631f14078a1f55
Dallin Romney <dallinromney@gmail.com>
2023-05-18 21:22:37 -0600

    babel aliases, small gear png

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo']
-  };
-};
+    presets: ['babel-preset-expo'],
+    alias: {
+      '^@src/(.+)': './src/\\1',
+      '^@assets/(.+)': './src/assets/\\1',
+      '^@components/(.+)': './src/components/\\1',
+      '^@contexts/(.+)': './src/contexts/\\1',
+      '^@models/(.+)': './src/models/\\1',
+      '^@navigation/(.+)': './src/navigation/\\1',
+      '^@screens/(.+)': './src/screens/\\1',
+      '^@services/(.+)': './src/services/\\1',
+      '^@utils/(.+)': './src/utils/\\1',
+    },
+  }
+}
